1443b538602f17fb430d712648109e13e36bace2
Christian Ziller <christian.r.ziller@gmail.com>
2026-01-11 16:17:17 -0500

    feat: add custom icon metadata for improved branding in application

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Playoff Fantasy Football",
   description: "Created by Christian Ziller",
+  icons: {
+    icon: {
+      url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🏈</text></svg>',
+      type: 'image/svg+xml',
+    },
+  },
 };
 
 export default function RootLayout({
